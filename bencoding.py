ae7db0895edf406b44bf327e523e59290b1d1650
yuval hadar <yuvalhad12@gmail.com>
2022-12-20 01:15:55 +0200

    added some guidelines

--- bencoding.py
+++ bencoding.py
@@ -10,7 +10,9 @@ def _decode(bencoded_bytes: bytes, decoded_dict: dict, start_search_index: int =
     if bencoded_bytes[start_search_index] == b'i':
         pass
     
-    if bencoded_bytes[start_search_index].isnumber
+    if bencoded_bytes[start_search_index] > ord('0') and bencoded_bytes[start_search_index] <= ord('9'):
+        # then we need all bytes from that index until one before :
+        pass
     #
 def decode(bencoded_bytes):
     decoded_dict = dict()
