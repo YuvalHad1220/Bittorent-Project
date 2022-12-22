40272da228a80a4a94792d5be11061e2600c8587
yuval hadar <yuvalhad12@gmail.com>
2022-12-22 21:07:40 +0200

    finding piece size algorithm + tests is working

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 from bencoding import encode, decode
 import random, string
+from utils import sizes, return_piece_size
 
 
 class TestBencode(unittest.TestCase):
@@ -58,6 +59,38 @@ class TestBencode(unittest.TestCase):
         print(test_list)
         
         self.assertEqual(str(test_list), str(decoded))
+
+class testUtils(unittest.TestCase):
+    def test_piece_size(self):
+        # we want to make sure we get smallest pieces size available
+        file_size_to_256kib = 256 * sizes['Kib'] * 5 * sizes['MiB'] - 1 # until here it will always be 256kilobibits to have less than 15MiB
+        file_size_to_512kib =  256 * sizes['Kib'] * 5 * sizes['MiB']  - 1 # until here it will always be 512 kib
+        file_size_to_1mib =  1 * sizes['Mib'] * 5 * sizes['MiB']  - 1 # same thing...
+        file_size_to_2mib =  2 * sizes['Mib'] * 5 * sizes['MiB']  - 1 # until here it will always be 512
+        file_size_to_4mib =  4 * sizes['Mib'] * 5 * sizes['MiB']  - 1 # until here it will always be 512
+        file_size_to_8mib =  8 * sizes['Mib'] * 5 * sizes['MiB']  - 1 # until here it will always be 512
+        file_size_to_16mib =  16 * sizes['Mib'] * 5 * sizes['MiB']  - 1 # until here it will always be 512
+        file_size_to_32mib =  32 * sizes['Mib'] * 5 * sizes['MiB']  - 1 # until here it will always be 512
+
+        """
+        All of those pieces size should result to <= 41943040 pieces. Obviously that algorithm needs much more improvment but it's a start
+        """
+
+
+        self.assertAlmostEqual(file_size_to_256kib / return_piece_size(file_size_to_256kib)['size'], 41943040, places=4)
+        self.assertAlmostEqual(file_size_to_512kib / return_piece_size(file_size_to_512kib)['size'], 41943040, places=4)
+        self.assertAlmostEqual(file_size_to_1mib / return_piece_size(file_size_to_1mib)['size'], 41943040, places=4)
+        self.assertAlmostEqual(file_size_to_2mib / return_piece_size(file_size_to_2mib)['size'], 41943040, places=4)
+        self.assertAlmostEqual(file_size_to_4mib / return_piece_size(file_size_to_4mib)['size'], 41943040, places=4)
+        self.assertAlmostEqual(file_size_to_8mib / return_piece_size(file_size_to_8mib)['size'], 41943040, places=4)
+        self.assertAlmostEqual(file_size_to_16mib / return_piece_size(file_size_to_16mib)['size'], 41943040, places=4)
+        self.assertAlmostEqual(file_size_to_32mib / return_piece_size(file_size_to_32mib)['size'], 41943040, places=4)
+
+
+
+        
+
+
 
 if __name__ == "__main__":
     unittest.main()
--- utils.py
+++ utils.py
@@ -1,5 +1,38 @@
 """
-maximum piece size - 32KiB. We want pieces to be as small as possible yet we want the total torrent file to be smaller than 200Mib
+maximum piece size - 32MiB (last return). We want pieces to be as small as possible (starting from 256kb) yet we want the total torrent file to be smaller than 15Mib
+https://www.reddit.com/r/torrents/comments/dzxfz1/2019_whats_ideal_piece_size/ 
+
+NOTE: need to configure to use si\decimal method
 """
+
+sizes = {
+    "b": 1,
+    "Kib": 1024,
+    "Mib": 1024 ** 2,
+    "Gib": 1024 ** 3,
+    "Tib": 1024 ** 4,
+    "KiB": 1024 * 8,
+    "MiB": 1024 ** 2 * 8,
+    "GiB": 1024 ** 3 * 8,
+    "TiB": 1024 ** 4 * 8,
+}
+
+
 def return_piece_size(file_size):
-    return {"size": 123123}
+    piece_sizes = [
+        256 * sizes['Kib'],
+        512 * sizes['Kib'],
+        1 * sizes['Mib'],
+        2 * sizes['Mib'],
+        4 * sizes['Mib'],
+        8 * sizes['Mib'],
+        16 * sizes['Mib'],
+    ]
+
+    for size in piece_sizes:
+        # we want MiB not Mib
+        if file_size / size < 5 * sizes['MiB']:
+            return {"size": size}
+    
+    return {"size": 32 * sizes['Mib']}
+
