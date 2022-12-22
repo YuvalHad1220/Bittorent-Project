ae721e31d7dff714341eff134e7f5485b31b2430
yuval hadar <yuvalhad12@gmail.com>
2022-12-22 17:16:33 +0200

    json settings

---
+++ utils.py
@@ -0,0 +1,5 @@
+"""
+maximum piece size - 32KiB. We want pieces to be as small as possible yet we want the total torrent file to be smaller than 200Mib
+"""
+def return_piece_size(file_size):
+    return {"size": 123123}
