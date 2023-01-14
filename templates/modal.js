18620ea1715be030f24025f7472fa81247b04606
yuval hadar <yuvalhad12@gmail.com>
2023-01-14 18:39:37 +0200

    finally choosing byte type works

--- templates/modal.js
+++ templates/modal.js
@@ -46,39 +46,10 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
-  function humanFileSize(bytes, si=true) {
-    const thresh = si ? 1000 : 1024;
-    const units = si ? ['KB', 'MB']
-                     : ['KiB', 'MiB'];
-    let u = -1;
-    do {
-      bytes /= thresh;
-      ++u;
-    } while (bytes >= thresh && u < units.length - 1);
-    return [Math.round(bytes * 10) / 10, units[u]];
-  }
+let unitType = null;
+let size_in_bytes = 0;
 
-  function fromHumanFileSize(humanSize) {
-    // Split the string into the number and the unit
-    const [numString, unit] = humanSize.split(/\s/);
-    const num = parseFloat(numString);
-  
-    // Look up the conversion factor for the unit
-    let factor = 1;
-    switch (unit) {
-      case 'B': factor = Math.pow(10, 0); break;
-      case 'KB': factor = Math.pow(10, 3); break;
-      case 'MB': factor = Math.pow(10, 6); break;
-      case 'KiB': factor = Math.pow(2, 10); break;
-      case 'MiB': factor = Math.pow(2, 20); break;
-    }
-  
-    // Return the number of bytes
-    return num * factor;
-  }
-
-
-function fetch_from_server(){
+async function fetch_from_server(){
   fetch('http://127.0.0.1:12345/edit_settings',
   {
     method: 'GET',
@@ -91,42 +62,84 @@ function fetch_from_server(){
   document.getElementById('defaultdownloadPath').value = jsoned_resp['default_download_path'];
   document.getElementById('defaultArchivePath').value = jsoned_resp['default_file_archive_path'];
   document.getElementById('port').value = jsoned_resp['port'];
-  document.getElementById('byteType').value = jsoned_resp['size_calc'];
-  handle_bytes_to_readable(jsoned_resp['max_torrentx_file_size']);
+  unitType = jsoned_resp['size_calc'];
+  document.getElementById('byteType').value = unitType; 
+  size_in_bytes = jsoned_resp['max_torrentx_file_size'];
+  update_type();
   });
 }
-
-function updateUnitOptions(unitType) {
+function updateUnitOptions() {
   const select = document.getElementById('fileSizeByteType');
   let units;
-
   if (unitType === 'SI') {
     units = ['KB', 'MB'];
-  } else {
+  } 
+  else {
     units = ['KiB', 'MiB'];
   }
-
   select.innerHTML = ''; // Clear existing options
-
   units.forEach((unit) => {
     const option = document.createElement('option');
     option.value = unit;
     option.textContent = unit;
     select.appendChild(option);
-  });
+    });
+}  
+function update_type(){
+  unitType = document.getElementById('byteType').value;
+  // if user changes type, than maybe he also changed value so update size_in_bytes
+  updateUnitOptions();
+  var inputed_bytes_as_str = document.getElementById("TorrentxSize").value;
+  if (inputed_bytes_as_str !== ""){
+    update_bytes(inputed_bytes_as_str);
+
+  }
+  display_bytes();
+}
+
+function update_bytes(new_bytes){
+  var value_in_humanreadable = parseFloat(new_bytes);
+  var bytes = 0;
+  var selected_value = document.getElementById("fileSizeByteType").value;
+
+  // we want to get the value from the previous type, so we stich
+  switch (selected_value) {
+    case "KB":
+      bytes = value_in_humanreadable * Math.pow(2,20);
+      break;
+    case "MB":
+      bytes = value_in_humanreadable * Math.pow(2,30);
+
+        break;
+    case "KiB":
+      bytes = value_in_humanreadable * Math.pow(10,6);
+
+        break;
+    case "MiB":
+      bytes = value_in_humanreadable * Math.pow(10,12);
+
+        break;
+  }
+  size_in_bytes = bytes;
+}
+
+// we need to change it! There are two functions loaded into this page. how do we care take care of it?
+function humanFileSizeLocal(bytes) {
+  const thresh = unitType == "SI" ? 1000 : 1024;
+  const units = unitType == "SI" ? ['KB', 'MB']: ['KiB', 'MiB'];
+  let u = -1;
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (bytes >= thresh && u < units.length - 1);
+  return [Math.round(bytes * 10) / 10, units[u]];
+}
+
+function display_bytes(){
+  var res_arr = humanFileSizeLocal(size_in_bytes);
+  alert(res_arr);
+  document.getElementById('TorrentxSize').value = res_arr[0];
+  document.getElementById('fileSizeByteType').value = res_arr[1];
 
 }
 
-function handle_bytes_to_readable (bytes, type){
-  // that function always gets bytes, type and as a result will update
-  // options type, readable size (value and mb\kb)
-  updateUnitOptions(unitType);
-  var values_arr = humanFileSize(bytes, type == "SI");
-  document.getElementById('TorrentxSize').value = values_arr[0];
-  document.getElementById('fileSizeByteType').value = values_arr[1];
-  
-}
-
-function on_type_change(){
-  // if now were
-}
